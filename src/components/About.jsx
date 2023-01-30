f1d9acf6cca7c008dd9c446750dea5a6ede1a01e
Rijan Shrestha <rijanshrestha98450@gmail.com>
2023-01-30 11:55:08 +0545

    jan30 2nd commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,16 +4,16 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 via-black to-gray-800 text-white"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 via-black to-gray-800 text-white pb-[150px]"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          <p className="text-4xl font-bold text-center md:text-left md:text-5xl">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-20">
+        <p className="text-xl">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
           deserunt illum mollitia officiis qui exercitationem perferendis neque
           quasi a recusandae necessitatibus tempora iusto! Blanditiis error
