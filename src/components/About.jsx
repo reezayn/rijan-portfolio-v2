ee773a9b9c104bcba0e66b84e0ea44b091fa365c
Rijan Shrestha <rijanshrestha98450@gmail.com>
2023-02-02 13:36:34 +0545

    feb2 2nd commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 const About = () => {
   return (
@@ -14,13 +15,13 @@ const About = () => {
         </div>
 
         <p className="text-xl">
-          Allow me to introduce myself, my name is Rijan Shrestha, a determined computer science student who has a deep love for all things technology. I am a full-stack enthusiast, constantly seeking new ways to push the boundaries of what's possible and grow my skills. As a student of computer science, I bring a unique perspective to each and every project I work on. My ability to turn ideas into tangible solutions has earned me a reputation as a problem solver with a creative streak. Whether working as part of a team or flying solo, I consistently strive to deliver outstanding results that exceed expectations. So, if you're in need of a talented, motivated, and innovative developer, look no further. I am here to bring your ideas to life.
+          Allow me to introduce myself, again. 😆 My name is Rijan Shrestha, a determined computer science student who has a deep love for all things technology. I am a full-stack enthusiast, constantly seeking new ways to push the boundaries of what's possible and grow my skills. As a student of computer science, I bring a unique perspective to each and every project I work on. My ability to turn ideas into tangible solutions has earned me a reputation as a problem solver with a creative streak. Whether working as part of a team or flying solo, I consistently strive to deliver outstanding results that exceed expectations. So, if you're in need of a talented, motivated, and innovative developer, look no further. I am here to bring your ideas to life.
         </p>
 
         <br />
 
         <p className="text-xl">
-        To get in touch with me, you can use the links provided at the bottom of this page or through the floating dock on the left. If you need to reach me quickly, you can find me on Instagram under the username <a href="https://www.instagram.com/ree_zayn/" target="_blank" rel="noreferrer"><button className="text-sky-400 cursor-pointer hover:underline">ree_zayn</button></a> , where I'm most active.
+        To get in touch with me, you can use the <button className="text-sky-400 cursor-pointer hover:underline"><Link to='contact' smooth duration={500}>contact links</Link></button> provided at the bottom of this page or through the floating dock on the left. If time is of essence and you need to reach me quickly, you can find me on Instagram under the username : <a href="https://www.instagram.com/ree_zayn/" target="_blank" rel="noreferrer"><button className="text-sky-400 cursor-pointer hover:underline">ree_zayn</button></a> , where I'm most active.
         </p>
       </div>
     </div>
